7c39f02d1e5c4532d6cfa078469cf356f9227dfd
ApTyyyP <arturhelpme@gmail.com>
2021-03-11 21:13:02 +0200

    Homework 2
    Это было сложновато для меня.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import bg from './assets/bg.jpg';
+
+const App = () => {
+    return (
+        <>
+            <Header
+                title="Hello React"
+                descr="This is block Header"
+            />
+            <Layout
+                title="Layout_1"
+                descr="This is block Layout 1"
+                urlBg={bg}
+            />
+            <Layout
+                title="Layout_2"
+                descr="This is block Layout 2"
+                colorBg="red"
+            />
+            <Layout
+                title="Layout_3"
+                descr="This is block Layout 3"
+                urlBg={bg}
+            />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+﻿
+/*import s from './style.module.css';
+
+import { ReactComponent as ReactLogo } from '../../assets/logo.svg';
+
+const Header = ({ title, descr }) => {
+    return (
+        <div className={s.root} style={styleRoot}>
+            <div className={s.container}>
+                <ReactLogo />
+                {
+                    title && (<h1 className={s.header}>
+                        {title}
+                    </h1>)
+                }
+                {descr && <p className={s.paragraph}>{descr}</p>}
+            </div>
+        </div>
+    )
+}*/
+
+import s from './style.module.css';
+
+const Header = ({ title, descr }) => {
+    return (
+        <header className={s.root}>
+            <div className={s.forest}></div>
+            <div className={s.container}>
+                <h1>This is title</h1>
+                <p>This is Description!</p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
